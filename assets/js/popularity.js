b2a8b7c6d5390aca1631f346646314c287d8e943
choi jang wook <jangwookchoi1@gmail.com>
2026-05-03 21:11:46 +0900

    Update popularity.js

--- assets/js/popularity.js
+++ assets/js/popularity.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 let key = "views_" + window.location.pathname;
 let count = localStorage.getItem(key) || 0;
+
 count++;
 localStorage.setItem(key, count);
 
