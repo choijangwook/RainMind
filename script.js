d874c12eb03b75a98c84a2192bd8ab571f706ea0
choi jang wook <jangwookchoi1@gmail.com>
2026-05-03 22:15:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-let isPlaying = false;
-
-const sounds = {
-  rain: new Audio("sounds/rain.mp3"),
-  cafe: new Audio("sounds/cafe.mp3"),
-  wind: new Audio("sounds/wind.mp3"),
-  thunder: new Audio("sounds/thunder.mp3"),
-  drone: new Audio("sounds/drone.mp3")
-};
-
-Object.values(sounds).forEach(audio => {
-  audio.loop = true;
-  audio.volume = 0;
-});
-
 document.addEventListener("DOMContentLoaded", () => {
 
+  let isPlaying = false;
   const btn = document.getElementById("mainBtn");
 
-  btn.addEventListener("click", async () => {
+  btn.addEventListener("click", () => {
 
     if (!isPlaying) {
-      for (let audio of Object.values(sounds)) {
-        try { await audio.play(); } catch(e) {}
-      }
       btn.innerText = "⏸ Stop";
       isPlaying = true;
     } else {
-      Object.values(sounds).forEach(a => a.pause());
       btn.innerText = "▶ Start";
       isPlaying = false;
     }
 
-  });
-
-  document.querySelectorAll("input[type=range]").forEach(slider => {
-    slider.addEventListener("input", (e) => {
-      const name = e.target.dataset.sound;
-      if (sounds[name]) {
-        sounds[name].volume = parseFloat(e.target.value);
-      }
-    });
   });
 
 });
